395b382d04b8467d6194bbf87abfd0f515971115
Paaru13 <parvatha33@gmail.com>
2022-06-27 16:01:42 +0530

    game over

--- src/App.js
+++ src/App.js
@@ -1,66 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState} from 'react'
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import _ from "lodash";
 
 function App() {
-  const [playerOne, setPlayerOne] = useState('X');
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [cellArray, setCellArray] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const [result, setResult] = useState("");
+
+  const checkResult = (checkArray) => {
+    const winningPositions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    const winningPosition = _.find(winningPositions, (array) => {
+      return (
+        checkArray[array[0]] === checkArray[array[1]] &&
+        checkArray[array[1]] === checkArray[array[2]] &&
+        checkArray[array[0]] !== null
+      );
+    });
+    if (winningPosition !== undefined) {
+      return checkArray[winningPosition[0]];
+    } else {
+      if (_.every(checkArray, (array) => array !== null)) {
+        return "draw";
+      } else {
+        return null;
+      }
+    }
+  };
+
+  const handleClick = (e, cellIndex) => {
+    setCellArray((array) => {
+      const newArray = [...array];
+      if (newArray[cellIndex] === null) {
+        newArray[cellIndex] = currentPlayer;
+        if (currentPlayer === "X") {
+          setCurrentPlayer("O");
+        } else {
+          setCurrentPlayer("X");
+        }
+      }
+      setResult(checkResult(newArray));
+      
+      return newArray;
+    });
+  };
 
   return (
-    <div>
-    <div className="container">
-      <div className="row">
-        <div className="cell">
-          <div className="cell-content">
-            
-          </div>
-        </div>
-        <div className="cell">
-          <div className="cell-content">
-            
-          </div>
-        </div>
-        <div className="cell">
-          <div className="cell-content">
-            
-          </div>
-        </div>
-        <div className="cell">
-          <div className="cell-content">
-            
-          </div>
-        </div>
-        <div className="cell">
-          <div className="cell-content">
-            
-          </div>
-        </div>
-        <div className="cell">
-          <div className="cell-content">
-            
-          </div>
-        </div>
-        <div className="cell">
-          <div className="cell-content">
-            
-          </div>
-        </div>
-        <div className="cell">
-          <div className="cell-content">
-            
-          </div>
-        </div>
-        <div className="cell">
-          <div className="cell-content">
-            
-          </div>
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          {_.map(cellArray, (el, idx) => {
+            return (
+              <div key={idx} className="cell">
+                <div
+                  onClick={(e) => handleClick(e, idx)}
+                  className="cell-content"
+                >
+                  {el}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
+      <div className="play-comment">
+        {(result !== null) ? <h1>Result :{result}</h1> : <h1>Current Player:{currentPlayer}</h1>}
+      </div>
     </div>
-    <h1>
-    Current Player: {playerOne}
-    </h1>
-  </div>
   );
 }
-
 export default App;
